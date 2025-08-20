d3282b4cb8b99ba3328662f1ad817746b95c207f
Hemant <hpatel571989@gmail.com>
2025-08-20 23:09:41 +0530

    Add source checkout stage

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -19,7 +19,7 @@ def call(Closure config) {
             ])
         }
         stage('Run knowedge builder') {
-            python3 print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+            sh 'python3 print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")'
         }
     }
     
