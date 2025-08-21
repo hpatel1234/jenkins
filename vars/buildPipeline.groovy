06bb62ea14202eaf2cfd56ad61ad16fda63b6853
Hemant <hpatel571989@gmail.com>
2025-08-21 23:22:49 +0530

    Add source checkout stage

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -52,9 +52,9 @@ def call(Closure config) {
         stage('Creating Pull request') {
             dir(repoName) {
                 sh 'bash -c "git checkout main && git checkout -b auto-doc"'
-                sh 'bash -c "mkdir -p ../../documentation/generated/"'
                 def bin_dir='virtual_env/bin'
                 dir(bin_dir) {
+                    sh 'bash -c "mkdir -p ../../documentation/generated/"'
                     sh 'bash -c "mv documentation.html ../../documentation/generated/documentation.html"'
                 }
                 sh 'bash -c "git commit documentation/generated/documentation.html -m \"Auto Committed file\""'
