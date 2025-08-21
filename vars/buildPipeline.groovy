f9ef24f3b5332d6d83de5a735c6742f3d0acaddc
Hemant <hpatel571989@gmail.com>
2025-08-21 22:59:15 +0530

    Add source checkout stage

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -23,7 +23,7 @@ def call(Closure config) {
             }
 
         }
-        stage('Create knowledge graph') {
+        stage('Creating python virtual environmane') {
             dir(repoName) {
                 print("We are creating virtual environment please wait.")
                 sh 'python3 -m venv virtual_env'
@@ -31,11 +31,42 @@ def call(Closure config) {
                 dir(bin_dir) {
                     print("Environment created installing dependencies")
                     sh 'bash -c "source activate && pip3 install -r ../../documentation/requirements.txt && deactivate"'
-                    print("Dependencies installed, started creating knowledge graph")
+                    print("Dependencies installed.")
+                }
+            }
+
+        }
+        stage('Create knowledge graph and documentation') {
+            dir(repoName) {
+                def bin_dir='virtual_env/bin'
+                dir(bin_dir) {
+                    print("Creating knowledge graph")
                     sh 'bash -c "source activate && python3 ../../documentation/knowledge_graph_builder.py --repo ../../src/main/java && deactivate"'
                     print("Knowledge graph created, started creating documentation")
                     sh 'bash -c "source activate && python3 ../../documentation/main.py && deactivate"'
                 }
+            }
+
+        }
+        stage('Moving Auto generated documentation to source directory') {
+            dir(repoName) {
+                def bin_dir='virtual_env/bin'
+                dir(bin_dir) {
+                    sh 'bash -c "mv documentation.html ../../documentation/generated/documentation.html"'
+                }
+            }
+
+        }
+        stage('Creating Pull request') {
+            dir(repoName) {
+                sh 'bash -c "git checkout main && git checkout -b auto-doc"'
+                sh 'bash -c "git commit documentation/generated/documentation.html -m \"Auto Committed file\""'
+                withCredentials([usernamePassword(credentialsId: "GITHUB_CRED", usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
+                    sh """
+                            git push https://${GIT_USER}:${GIT_PASS}@github.com/hpatel1234/${repoName}.git HEAD:auto-doc
+                        """
+                }
+                //sh 'bash -c "git push -u origin auto-doc"'
             }
 
         }
